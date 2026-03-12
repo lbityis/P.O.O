ac5fdcb9dcbe24e8ac95cd7d9a8c07f751574303
Hani <elisasoto@liceovvh.cl>
2026-03-12 11:15:40 -0300

    .

---
+++ 03_bucles_for/static/js/script.js
@@ -0,0 +1,154 @@
+/*      1. El Contador en Pantalla
+
+Selecciona el párrafo con el id pantalla.
+Crea un ciclo for que vaya del 1 al 10.
+En cada vuelta del ciclo, agrega (concatena con +=) 
+el número actual seguido de un guion 
+(ej: "1 - 2 - 3 - ") al párrafo.*/
+
+function contadorPantalla() {
+    let numeros = [];
+    const container = document.getElementById('resultado-container');
+    const result1 = document.getElementById('result1');
+
+    for (let i = 1; i <= 10; i++) {
+        numeros.push(i);
+    }
+
+    result1.textContent = `Contando: ${numeros.join(" - ")}`;
+    container.classList.remove('d-none');
+}
+
+/*      2. Lista de Asistencia Automática
+
+Tienes la siguiente lista de estudiantes: 
+let curso = ["Ana", "Diego", "Sofía", "Matias"];
+Selecciona el párrafo pantalla y límpialo primero asignándole un texto vacío ("").
+Usa un ciclo for para recorrer el arreglo. 
+En cada iteración, agrega el nombre del estudiante al párrafo, 
+separándolos por una coma y un espacio.*/
+
+function asistencia() {
+    let curso = ["Ana", "Diego", "Sofía", "Matias"];
+    const container = document.getElementById('resultado-containerdos');
+    const result2 = document.getElementById('result2');
+    result2.textContent = "";
+    for (let i = 0; i < curso.length; i++) {
+        result2.textContent += `${curso[i]}, `;
+    }
+    container.classList.remove('d-none');
+}
+
+/*      3. Buscador de Aprobados (Escala 1 a 7)
+
+Tienes una lista con las notas finales de un curso:
+let notas = [3.5, 6.2, 5.0, 2.8, 4.5, 7.0];
+Crea una variable contadorAprobados que inicie en 0.
+Recorre el arreglo con un for. Si la nota es 4.0 o superior, 
+suma 1 al contador. Al terminar el ciclo, muestra en el párrafo 
+pantalla: "Total de alumnos aprobados: [número]".*/
+
+function aprobados() {
+    let notas = [3.5, 6.2, 5.0, 2.8, 4.5, 7.0];
+    let contadorAprobados = 0;
+    const container = document.getElementById('resultado-containertres');
+    const result3 = document.getElementById('result3');
+    for (let i = 0; i >= notas.length; i++) {
+        if (notas[i] >= 4.0) {
+            contadorAprobados++;
+        }
+    }
+    result3.textContent = "Total de alumnos aprobados: " + contadorAprobados;
+    container.classList.remove('d-none');
+}
+
+/*      4. El Filtro de Inventario
+
+Una tienda quiere mostrar solo los productos que están disponibles.
+let productos = ["Teclado", "Mouse", "Agotado", "Monitor", "Agotado", "Audífonos"];
+Recorre la lista con un ciclo for.
+Si el producto es diferente de "Agotado" (!==),
+agrégalo al texto del párrafo pantalla. Si dice "Agotado", sáltalo.*/
+
+function inventario() {
+    let productos = ["Teclado", "Mouse", "Agotado", "Monitor", "Agotado", "Audífonos"];
+    const container = document.getElementById('resultado-containercuatro');
+    const result4 = document.getElementById('result4');
+    result4.textContent = "";
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i] !== "Agotado") continue;
+        result4.textContent = `${productos[i]}`;
+    }
+    container.classList.remove('d-none');
+}
+
+/*      5. Sumando la Colecta
+
+Durante una actividad escolar se recolectaron distintos montos en dinero:
+let aportes = [1500, 2000, 500, 3000, 1000];
+Crea una variable totalRecaudado que inicie en 0.
+Usa un ciclo for para sumar cada uno de los aportes a la variable total.
+Al finalizar el ciclo, escribe en el párrafo:
+"La colecta reunió un total de: $[total]".*/
+
+function colecta() {
+    let numeros = [];
+    const container = document.getElementById('resultado-containercinco');
+    const result5 = document.getElementById('result5');
+
+    result5.textContent = `Contando: ${numeros.join(" - ")}`;
+    container.classList.remove('d-none');
+}
+
+/*      6. Formateador de Nombres VIP
+
+Tienes una lista de usuarios: let asistentes = ["carlos", "MARIA", "pedro", "LUCIA"];
+Queremos mostrarlos en el HTML, pero destacando a algunos.
+Recorre el arreglo. Si el índice actual (i) es par 
+(puedes usar i % 2 === 0), agrega la palabra " [VIP]" al lado de su nombre 
+y muéstralo en el párrafo. Si es impar, muéstralo normal.*/
+
+function vip() {
+    let numeros = [];
+    const container = document.getElementById('resultado-containerseis');
+    const result6 = document.getElementById('result6');
+
+    result6.textContent = `Contando: ${numeros.join(" - ")}`;
+    container.classList.remove('d-none');
+}
+
+/*      7. El Buscador de Stock Específico
+
+Tienes un inventario: let bodega = ["Lápiz", "Cuaderno", "Goma", "Cuaderno", "Regla", "Cuaderno"];
+Crea una variable articuloBuscado = "Cuaderno" y una variable vecesEncontrado = 0.
+Recorre la bodega con un for. Cada vez que el elemento de la lista sea igual al articuloBuscado,
+aumenta tu contador. Al final, inyecta en el párrafo: 
+"El artículo [articuloBuscado] se encuentra [veces] veces en la bodega".*/
+
+function stock() {
+    let numeros = [];
+    const container = document.getElementById('resultado-containersiete');
+    const result7 = document.getElementById('result7');
+
+    result7.textContent = `Contando: ${numeros.join(" - ")}`;
+    container.classList.remove('d-none');
+}
+
+/*      8. Generador de Párrafos de Advertencia
+
+Tienes una lista de temperaturas registradas en una sala de servidores:
+let temperaturas = [22, 24, 28, 35, 21, 38];
+Queremos mostrar alertas solo para las temperaturas peligrosas.
+Usa .innerHTML en lugar de .textContent en tu párrafo.
+Recorre el arreglo. Si la temperatura es mayor a 30, inyecta en el párrafo:
+"¡ALERTA! Temperatura crítica de " + temperatura + " grados.<br>".
+(Nota: el <br> hará que cada alerta aparezca en una nueva línea en la pantalla).*/
+
+function advertencia() {
+    let numeros = [];
+    const container = document.getElementById('resultado-containerdos');
+    const result2 = document.getElementById('result2');
+
+    result2.textContent = `Contando: ${numeros.join(" - ")}`;
+    container.classList.remove('d-none');
+}
